944a400ca1cd5addbf06cdfc08c28a3685d0d829
Melty5 <100410311+Melty5@users.noreply.github.com>
2022-02-25 18:33:44 +0900

    Update Roll_Shuffle.py

--- Roll_Shuffle.py
+++ Roll_Shuffle.py
@@ -6,25 +6,17 @@ import random
 roll_list = ["TOP","JG","MID","ADC","SUP"]
 
 st.title("LOL自動ロール振り分け")
-st.write("""
-    ### 今んとこフルパでしか対応してない
-     """)
+
 players = st.text_area("1人ずつ改行してプレイヤー入力しやがれ",height=140)
 
-if st.button("振り分け"):
-    try:     
-        player_list = []
-        player_list=players.splitlines()
-        random.shuffle(player_list)
+if st.button("振り分け"):  
+    player_list = []
+    player_list=players.splitlines()
+    random.shuffle(player_list)
 
-        roll_table = pd.DataFrame({
-            "Roll" : roll_list,
-            "Player" : player_list
-        })
-
-        col_list=st.columns(5)
-        for i in range(5):
-            col_list[i].metric(label=roll_list[i],value=player_list[i])
+    n = len(player_list)
+    roll_list = random.sample(roll_list,n)
+    col_list=st.columns(n)
+    for i in range(n):
+        col_list[i].metric(label=roll_list[i],value=player_list[i])
             
-    except:
-        st.write("人数おかしい")
